30d78b5a87f45ba03c248208224624d44e8142e2
loading <dwzzhw@126.com>
2018-11-07 17:35:30 +0800

    add GLES20Activity from api demo

--- app/src/main/java/loading/com/helloopengl/MainActivity.java
+++ app/src/main/java/loading/com/helloopengl/MainActivity.java
@@ -1,5 +1,6 @@
 package loading.com.helloopengl;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,10 +8,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import loading.com.helloopengl.apidemo.GLES20Activity;
 import loading.com.helloopengl.utils.Loger;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
     private TextView mTipsView;
     private Button mTestBtn01;
 
@@ -24,14 +27,17 @@ public class MainActivity extends AppCompatActivity {
         mTestBtn01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                doTest();
+                openGLES20PageFromApiDemo();
                 mTipsView.setText("Test Button 01 is clicked.");
             }
         });
     }
 
-    private void doTest() {
-        Loger.d(TAG, "-->doTest()");
-        Toast.makeText(this, "Do Test clicked", Toast.LENGTH_SHORT).show();
+    private void openGLES20PageFromApiDemo() {
+        Loger.d(TAG, "-->openGLES20PageFromApiDemo()");
+        Toast.makeText(this, "openGLES20PageFromApiDemo", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, GLES20Activity.class);
+        startActivity(intent);
     }
 }
